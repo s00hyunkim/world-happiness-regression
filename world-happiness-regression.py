4dd9e9fa55168430dbde8fba51ada5e62abe01fd
Soohyun <ksh731400@gmail.com>
2022-07-06 19:27:18 -0400

    edited some parts

--- world-happiness-regression.py
+++ world-happiness-regression.py
@@ -2,94 +2,24 @@
 ## libraried installed so far: numpy, pandas, sklearn, matplotlib
 import pandas as pd
 from sklearn.impute import KNNImputer
-import numpy as np
-# import itertools
-# import copy
-
-# import random
-# random.seed(0)
-
-# from sklearn import preprocessing
-# from sklearn.model_selection import train_test_split
-# import seaborn as sns
-
-# from statistics import mean
-
-# from sklearn import datasets
-# from sklearn.preprocessing import LabelEncoder
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.model_selection import cross_val_score
-# from sklearn import metrics
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.preprocessing import StandardScaler
-
-# import matplotlib.pyplot as plt
-# import matplotlib.gridspec as gridspec
-
-# from mlxtend.plotting import plot_decision_regions
-# from mlxtend.preprocessing import shuffle_arrays_unison
-# import seaborn as sns
-
-# from tqdm.notebook import tqdm_notebook as tqdm
-
-# from sklearn.preprocessing import PolynomialFeatures
-# from sklearn.metrics import mean_squared_error
-# import operator
-# import matplotlib.pyplot as plt
-# from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 # from sklearn.linear_model import Ridge
 # from sklearn.linear_model import Lasso
 # from sklearn.linear_model import RidgeCV
 # from sklearn.linear_model import LassoCV
-# from sklearn.linear_model import SGDRegressor
-# from sklearn.neighbors import KNeighborsRegressor
-
-# from math import log2, sqrt, isnan
-# from sklearn.preprocessing import LabelEncoder
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn import metrics
-# from sklearn import tree
-# from sklearn import preprocessing
-# import matplotlib.pyplot as plt
-# import six
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.tree import DecisionTreeRegressor
-# from sklearn.ensemble import RandomForestRegressor
-# from sklearn.ensemble import AdaBoostRegressor
-# from sklearn.metrics import make_scorer
-# from sklearn.tree import export_graphviz
-# from six import StringIO  
-# from IPython.display import Image  
-# import pydotplus
-# from tqdm.notebook import tqdm_notebook as tqdm
-# from sklearn.model_selection import GridSearchCV
-
-# from sklearn.preprocessing import Normalizer
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.metrics import accuracy_score
-# from sklearn.model_selection import KFold
-# from collections import Counter
-
-# from sklearn.metrics import accuracy_score
-# from sklearn.neighbors import KNeighborsClassifier
-# from keras.models import Sequential
-# from keras.layers import Dense
-# import matplotlib.pyplot as plt
-# from keras.layers import Dropout
-# from keras import regularizers
-# from keras import backend as K
-# from keras import callbacks
-
-
-# from sklearn.metrics import r2_score
-# from sklearn.decomposition import PCA
-# from sklearn.feature_selection import RFE
+from sklearn.metrics import r2_score
+import numpy as np
 
 # STEP 1: IMPORT DATASET
 # 1-1. import csv file
 data = pd.read_csv('world-happiness-score-2020.csv')
 # 1-2. check if the dataset has been imported successfully
-data.head()
+print('[SNIPPET OF THE DATASET]')
+print(data.head())
+print('')
 
 # STEP 2: CLEAN & MANIPULATE DATA
 # 2-1. remove columns that we don't need
@@ -101,9 +31,11 @@ axis = 1, inplace = True)
 # 2-2. rename columns
 data.columns = ['Ladder', 'LogGDP', 'SocialSupport', 'HealthyLifeExpectancy', 'Freedom', 'Generosity', 'Corruption']
 # 2-3. check if there are any null values
-data.isnull().sum()
+print('[# OF NULL VALUES OF EACH FEATURE]')
+print(data.isnull().sum())
+print('')
 # 2-4. determine what % of each feature is null to figure out which null values to drop
-print('[% OF NULL VALUES OF EACH FEATURE')
+print('[% OF NULL VALUES OF EACH FEATURE]')
 for col in data.columns:
     print(col,  ': %.2f%%' %((data[col].isnull().sum() / data.shape[0]) * 100))
 print('')
@@ -113,17 +45,23 @@ data = data[data[''].notna()]
 data = data[data[''].notna()]
 data = data[data[''].notna()]
 # 2-6. check how many non-null values each feature has
-data.info()
+print('[# OF NON-NULL VALUES OF EACH FEATURE]')
+print(data.info())
+print('')
 # 2-7. for other null values, use euclidean distance & uniform weights
 #      to fill the null values with the mean of k (= 40) nearest neighbours
 imputer = KNNImputer(n_neighbors = 40, copy = False) # copy is set to False to make imputation happen in place
 imputer.fit_transform(data)
 # 2-8. check how many non-null values each feature has
 #      (all feature should have the same # of non-null values at this step)
-data.info()
+print('[# OF NON-NULL VALUES OF EACH FEATURE]')
+print(data.info())
+print('')
 # 2-9. check correlation between each feature and target
 #      to see if we need to drop more before getting into modelling
-data.corr()['Ladder']
+print('[CORRELATION BETWEEN FEATURES AND SCORE]')
+print(data.corr()['Ladder'])
+print('')
 
 # STEP 3: SPLIT DATA
 # 3-1. set X to be all features and y to be target
@@ -133,4 +71,13 @@ y = data.iloc[:, 0]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 101)
 
 # STEP 4: BUILD REGRESSION MODEL
-
+# 4-1. normalize data
+scaler = Normalizer().fit(X_train)
+N_X_train = scaler.transform(X_train)
+N_X_test = scaler.transform(X_test)
+# 4-2. standardize data
+scaler = StandardScaler().fit(X_train)
+S_X_train = scaler.transform(X_train)
+S_X_test = scaler.transform(X_test)
+# 4-3. create different types of regression model with original data
+print('[WITH NEITHER NORMALIZATION NOR STANDARDIZATION]')
