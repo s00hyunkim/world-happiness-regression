f8418dfe22002749b2290cc9669185c07efc9b6f
Soohyun <ksh731400@gmail.com>
2022-07-06 22:00:25 -0400

    finished coding

--- world-happiness-regression.py
+++ world-happiness-regression.py
@@ -6,20 +6,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
-# from sklearn.linear_model import Ridge
-# from sklearn.linear_model import Lasso
-# from sklearn.linear_model import RidgeCV
-# from sklearn.linear_model import LassoCV
+from sklearn.linear_model import Ridge
 from sklearn.metrics import r2_score
 import numpy as np
 
 # STEP 1: IMPORT DATASET
 # 1-1. import csv file
-data = pd.read_csv('world-happiness-score-2020.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/s00hyunkim/world-happiness-regression/main/world-happiness-score-2020.csv')
 # 1-2. check if the dataset has been imported successfully
-print('[SNIPPET OF THE DATASET]')
-print(data.head())
-print('')
+# print('[SNIPPET OF THE DATASET]')
+# print(data.head())
+# print('')
 
 # STEP 2: CLEAN & MANIPULATE DATA
 # 2-1. remove columns that we don't need
@@ -39,21 +36,7 @@ print('[% OF NULL VALUES OF EACH FEATURE]')
 for col in data.columns:
     print(col,  ': %.2f%%' %((data[col].isnull().sum() / data.shape[0]) * 100))
 print('')
-# 2-5. drop null values of the features that have the least percentage
-#      to avoid dropping lots of data entries
-data = data[data[''].notna()]
-data = data[data[''].notna()]
-data = data[data[''].notna()]
 # 2-6. check how many non-null values each feature has
-print('[# OF NON-NULL VALUES OF EACH FEATURE]')
-print(data.info())
-print('')
-# 2-7. for other null values, use euclidean distance & uniform weights
-#      to fill the null values with the mean of k (= 40) nearest neighbours
-imputer = KNNImputer(n_neighbors = 40, copy = False) # copy is set to False to make imputation happen in place
-imputer.fit_transform(data)
-# 2-8. check how many non-null values each feature has
-#      (all feature should have the same # of non-null values at this step)
 print('[# OF NON-NULL VALUES OF EACH FEATURE]')
 print(data.info())
 print('')
@@ -63,21 +46,44 @@ print('[CORRELATION BETWEEN FEATURES AND SCORE]')
 print(data.corr()['Ladder'])
 print('')
 
-# STEP 3: SPLIT DATA
-# 3-1. set X to be all features and y to be target
+# STEP 3: PLOT CORRELATION BETWEEN FEATURES AND HAPPINESS SCORE
+data.plot.scatter(x= 'LogGDP', y='Ladder')
+data.plot.scatter(x= 'SocialSupport', y='Ladder')
+data.plot.scatter(x= 'HealthyLifeExpectancy', y='Ladder')
+data.plot.scatter(x= 'Freedom', y='Ladder')
+data.plot.scatter(x= 'Generosity', y='Ladder')
+data.plot.scatter(x= 'Corruption', y='Ladder')
+
+# STEP 4: SPLIT DATA
+# 4-1. set X to be all features and y to be target
 X = data.iloc[:,1:]
 y = data.iloc[:, 0]
-# 3-2. split X and y into training and test sets
+# 4-2. split X and y into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 101)
 
-# STEP 4: BUILD REGRESSION MODEL
-# 4-1. normalize data
-scaler = Normalizer().fit(X_train)
-N_X_train = scaler.transform(X_train)
-N_X_test = scaler.transform(X_test)
-# 4-2. standardize data
-scaler = StandardScaler().fit(X_train)
-S_X_train = scaler.transform(X_train)
-S_X_test = scaler.transform(X_test)
-# 4-3. create different types of regression model with original data
+# STEP 5: BUILD REGRESSION MODELS
 print('[WITH NEITHER NORMALIZATION NOR STANDARDIZATION]')
+print('[STANDARD LINEAR REGRESSION]')
+reg = LinearRegression().fit(X_train, y_train)
+y_pred = reg.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+print('Standard linear regression R2 = ', r2, '\n')
+coef = reg.coef_
+print('Standard linear regression coefficients :')
+i = 0; 
+for col in X.columns:
+  print(col, ":", coef[i])
+  i = i + 1
+print('')
+print('[RIDGE REGRESSION]')
+ridge_reg = Ridge().fit(X_train, y_train)
+y_pred = ridge_reg.predict(X_test)
+r2 = r2_score(y_test, y_pred)
+print("Ridge linear regression R2 = ", r2, "\n")
+coef = reg.coef_
+print("Ridge linear regression coefficients :")
+i = 0; 
+for col in X.columns:
+  print(col, ":", coef[i])
+  i = i + 1
+print('')
